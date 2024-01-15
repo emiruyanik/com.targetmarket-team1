883139caf47707350944c3a43d81363d85624b72
yusuf <yusufemiruyanik.gmail.com>
2024-01-15 23:46:23 +0300

    T1-104-smartphone tab tests added our project

--- src/main/java/utils/Pages.java
+++ src/main/java/utils/Pages.java
@@ -3,19 +3,24 @@ package utils;
 import pages.HomePage;
 import pages.TargetMarketHomePage;
 import pages.TargetMarketLoginPage;
+import pages.target_market_homepage_tabs.SmartphoneTab;
 
 public class Pages {
 
-	public HomePage getHomePage() {
-		return new HomePage();
-	}
+    public HomePage getHomePage() {
+        return new HomePage();
+    }
 
-	public TargetMarketLoginPage getTargetMarketLoginPage() {
-		return new TargetMarketLoginPage();
-	}
+    public TargetMarketLoginPage getTargetMarketLoginPage() {
+        return new TargetMarketLoginPage();
+    }
 
-	public TargetMarketHomePage getTargetMarketHomePage() {
-		return new TargetMarketHomePage();
-	}
+    public TargetMarketHomePage getTargetMarketHomePage() {
+        return new TargetMarketHomePage();
+    }
+
+    public SmartphoneTab getSmartphoneTab() {
+        return new SmartphoneTab();
+    }
 
 }
