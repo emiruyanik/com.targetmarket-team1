73381742ec3f27f0aebebc7a4706c472e66cb60f
yusuf <yusufemiruyanik.gmail.com>
2024-01-17 20:45:32 +0300

    hello world

--- src/main/java/pages/target_market_homepage_tabs/GroceriesTab.java
+++ src/main/java/pages/target_market_homepage_tabs/GroceriesTab.java
@@ -8,50 +8,66 @@ import java.util.List;
 
 public class GroceriesTab extends TargetMarketHomePage {
 
-	@FindBy(css = ".card-body>h5")
-	private List<WebElement> groceriesTitles;
+    @FindBy(css = ".card-body>h5")
+    private List<WebElement> groceriesTitles;
 
-	@FindBy(css = ".text-danger>i")
-	private List<WebElement> groceriesPrices;
+    @FindBy(css = ".text-danger>i")
+    private List<WebElement> groceriesPrices;
 
-	@FindBy(css = ".btn-danger")
-	private List<WebElement> addToCartButton;
+    @FindBy(css = ".btn-danger")
+    private List<WebElement> addToCartButton;
 
-	public void placeOrder(String groceryName) {
-		List<String> groceries = groceriesTitles.stream().map(WebElement::getText).toList();
-		actions.moveToElement(addToCartButton.get(groceries.indexOf(groceryName)));
-		try {
-			addToCartButton.get(groceries.indexOf(groceryName)).click();
-		}
-		catch (Exception ex) {
-			addToCartButton.get(groceries.indexOf(groceryName)).click();
-		}
-	}
+    public void placeOrder(String groceryName) {
+        List<String> groceries = groceriesTitles.stream().map(WebElement::getText).toList();
+        actions.moveToElement(addToCartButton.get(groceries.indexOf(groceryName)));
+        try {
+            addToCartButton.get(groceries.indexOf(groceryName)).click();
+        } catch (Exception ex) {
+            addToCartButton.get(groceries.indexOf(groceryName)).click();
+        }
+    }
 
-	public boolean isSortedFromSmallToBigPrice() {
-		for (int i = 0; i < getIntegerOfPrices().size() - 1; i++) {
-			if (getIntegerOfPrices().get(i) > getIntegerOfPrices().get(i + 1)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public void placeOrder1(String groceryName) {
+        List<String> groceries = groceriesTitles.stream().map(WebElement::getText).toList();
+        actions.moveToElement(addToCartButton.get(groceries.indexOf(groceryName)));
+        try {
+            addToCartButton.get(groceries.indexOf(groceryName)).click();
+        } catch (Exception ex) {
+            addToCartButton.get(groceries.indexOf(groceryName)).click();
+        }
+    }
 
-	public boolean isSortedFromBigToSmallPrice() {
-		for (int i = 0; i < getIntegerOfPrices().size() - 1; i++) {
-			if (getIntegerOfPrices().get(i) < getIntegerOfPrices().get(i + 1)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public boolean isSortedFromSmallToBigPrice() {
+        for (int i = 0; i < getIntegerOfPrices().size() - 1; i++) {
+            if (getIntegerOfPrices().get(i) > getIntegerOfPrices().get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public List<Integer> getIntegerOfPrices() {
-		return groceriesPrices.stream().map(price -> Integer.parseInt(price.getText().substring(1))).toList();
-	}
+    public boolean isSortedFromBigToSmallPrice() {
+        for (int i = 0; i < getIntegerOfPrices().size() - 1; i++) {
+            if (getIntegerOfPrices().get(i) < getIntegerOfPrices().get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public List<String> getStringOfTitles() {
-		return groceriesTitles.stream().map(title -> title.getText().toLowerCase()).toList();
-	}
+    public List<Integer> getIntegerOfPrices() {
+        return groceriesPrices.stream().map(price -> Integer.parseInt(price.getText().substring(1))).toList();
+    }
+
+    public List<String> getStringOfTitles() {
+        deneme();
+        return groceriesTitles.stream().map(title -> title.getText().toLowerCase()).toList();
+
+    }
+
+    public void deneme() {
+        System.out.println("gitbash bize yemuk yaptı");
+    }
+
 
 }
