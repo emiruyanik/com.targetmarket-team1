35bd9354aa2e277e789b16c70d12ba2c47e12ecb
Gurol GOKYAR <gurolgokyar@gmail.com>
2024-01-17 15:31:33 +0300

    target market home page womens shoes tab structure created

--- src/main/java/pages/target_market_homepage_tabs/WomenShoesTab.java
+++ src/main/java/pages/target_market_homepage_tabs/WomenShoesTab.java
@@ -2,6 +2,30 @@ package pages.target_market_homepage_tabs;
 
 import pages.TargetMarketHomePage;
 
-public class WomenShoesTab extends TargetMarketHomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import java.util.List;
 
+public class WomenShoesTab extends TargetMarketHomePage {
+    @FindBy(css = ".card-title")
+    private List<WebElement> womenShoesTitles;
+
+    @FindBy(css = ".btn-danger")
+    private List<WebElement> addTheChartButtons;
+
+    @FindBy(css = ".text-danger > i")
+    private List<WebElement> womenShoesPrices;
+
+    public String PlaceOrder(String womenShoesNames) {
+        List<String> shoesNames = womenShoesTitles.stream().map(WebElement::getText).toList();
+        actions.moveToElement(addTheChartButtons.get(shoesNames.indexOf(womenShoesNames)));
+
+        try {
+            addTheChartButtons.get(shoesNames.indexOf(womenShoesNames));
+        } catch (Exception ex) {
+            addTheChartButtons.get(shoesNames.indexOf(womenShoesNames));
+        }
+
+        return womenShoesNames;
+    }
 }
