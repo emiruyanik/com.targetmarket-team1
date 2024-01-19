2835e9377b3f6856f6886b88e97aa9fbd19ac8a4
adakayaaa <ummuhaneltn@gmail.com>
2024-01-19 08:57:05 +0100

    T1-101-uptated furniture tab class and tests

--- src/test/java/TargetMarketFurnitureTests.java
+++ src/test/java/TargetMarketFurnitureTests.java
@@ -1,8 +1,11 @@
 import base_test.Hooks;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.BrowserUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TargetMarketFurnitureTests extends Hooks {
@@ -16,7 +19,7 @@ public class TargetMarketFurnitureTests extends Hooks {
 	 * Bed and Sofa for Coffe Cafe. 7- Click on cart button. 8- Verify products are
 	 * displayed on the cart.
 	 */
-	@Test(priority = 1)
+	@Test(priority = 0)
 	void testFurnitureTab() {
 		// 1- Go to https://test.inar-academy.com/target-market.
 		pages.getHomePage().clickOnTargetMarketLink();
@@ -34,6 +37,44 @@ public class TargetMarketFurnitureTests extends Hooks {
 
 	}
 
+	// Verify that sort 'Lowest price' functionality
+	@Test(priority = 1)
+	void isSortableFromSmallToBigPrices() {
+		pages.getTargetMarketHomePage().selectSortType("Lowest Price");
+		boolean isSortable = pages.getFurnitureTab().isSortedFromSmallToBigPrice();
+		Assert.assertTrue(isSortable, "These prices not sortable from small to big!");
+	}
+
+	// Verify that sort 'Highest price' functionality
+	@Test(priority = 1)
+	void isSortableFromBigToSmallPrices() {
+		pages.getTargetMarketHomePage().selectSortType("Highest Price");
+		boolean isSortable = pages.getFurnitureTab().isSortedFromBigToSmallPrice();
+		Assert.assertTrue(isSortable, "These prices not sortable from big to small!");
+	}
+
+	// Verify that sort 'A-Z price' functionality
+	@Test(priority = 1)
+	void isSortableFromAToZTitles() {
+		List<String> list = new ArrayList<>(pages.getFurnitureTab().getStringOfTitles());
+		Collections.sort(list);
+
+		// Select "A-Z" from "Sorted By" selector
+		pages.getTargetMarketHomePage().selectSortType("A-Z");
+
+		// Verify that this is sorted.
+		Assert.assertEquals(list, pages.getFurnitureTab().getStringOfTitles());
+	}
+
+	// Verify that sort 'Z-A price' functionality
+	@Test(priority = 1)
+	void isSortableFromZToATitles() {
+		List<String> list = new ArrayList<>(pages.getFurnitureTab().getStringOfTitles());
+		list.sort(Comparator.reverseOrder());
+		pages.getTargetMarketHomePage().selectSortType("Z-A");
+		Assert.assertEquals(list, pages.getFurnitureTab().getStringOfTitles());
+	}
+
 	@Test(priority = 2)
 	void testDisplayedOnTheCart() {
 		// 6- Click on cart of Mornadi Velvet Bed and Sofa for Coffe Cafe.
@@ -42,6 +83,7 @@ public class TargetMarketFurnitureTests extends Hooks {
 
 		// 7- Click on cart button.
 		pages.getFurnitureTab().clickOnCartButton();
+		BrowserUtils.wait(1);
 
 		// 8- Verify products are displayed on the cart.
 		List<String> list = pages.getFurnitureTab().getProductsNamesOnCart();
